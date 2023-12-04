27b6028fca752eef8b2cdfed3d5c21767e5fcbc2
crocs-in-socks <gouriswag1@gmail.com>
2023-12-04 23:57:27 +0530

    Actually fixed this time

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
           <Route exact path="/schedule" element={<Schedule />} />
           <Route exact path="/speakers" element={<SpeakerPage />} />
           <Route exact path="/" element={<Home />} /> 
-          {/* <Route path="*" element={<Home />} /> */}
+          <Route path="*" element={<Home />} />
         </Routes>
       </Router>
     </div>
