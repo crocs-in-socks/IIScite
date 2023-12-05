2b0c4cbb8385ab577adc08af195bedf38a00d5b2
crocs-in-socks <gouriswag1@gmail.com>
2023-12-05 11:55:32 +0530

    Made responsive

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -10,12 +10,12 @@ export default function Navbar() {
                         <div className="navbar-list-item">
                             <a href="/home" className="navbar-link">Home</a>
                         </div>
-                        <div className="navbar-list-item navbar-left">
+                        <div className="navbar-list-item">
                             <a href="/schedule" className="navbar-link">Schedule</a>
                         </div>
-                    </div> 
+                    </div>
                 </div>
-                <div className="navbar-list-item navbar-title">
+                <div className="navbar-title navbar-list-item">
                     WORKSHOP ON AI IN PRECISION MEDICINE
                 </div>
             </div>
--- frontend/src/components/SpeakerComponent.js
+++ frontend/src/components/SpeakerComponent.js
@@ -2,6 +2,7 @@ import React from "react";
 import Speaker from "./SpeakerCard";
 import "../styles/SpeakerComponent.css";
 import SpeakerImage1 from "../assets/images/speaker.jpeg";
+import SpeakerImage10 from "../assets/images/speaker10.jpg"
 
 export default function Speakers() {
     return(
@@ -13,6 +14,7 @@ export default function Speakers() {
                 </div>
                 <div className="speakercomponent-row">
                     <Speaker image={SpeakerImage1} name={"Prof. Phaneendra Yalavarthy, IISc Bengaluru"}/>
+                    <Speaker image={SpeakerImage10} name={"Mr. Dileep Mangsuli, Head, R&D Development centre, Siemens Healthineers"}/>
                 </div>
             </div>
         </>
