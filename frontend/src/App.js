0fa4bc1d5035e10ecb4b83ba34d209a516955b8b
crocs-in-socks <gouriswag1@gmail.com>
2023-12-05 10:40:33 +0530

    Removed Speaker Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Schedule from './pages/Schedule';
-import SpeakerPage from './pages/SpeakerPage';
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/schedule" element={<Schedule />} />
-          <Route exact path="/speakers" element={<SpeakerPage />} />
           <Route exact path="/" element={<Home />} /> 
           <Route path="*" element={<Home />} />
         </Routes>
