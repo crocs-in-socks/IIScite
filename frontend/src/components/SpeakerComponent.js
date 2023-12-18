a80b5019affe542503beb92e730c43418a6281a5
crocs-in-socks <gouriswag1@gmail.com>
2023-12-18 14:58:22 +0530

    Added more speakers

--- frontend/src/components/SpeakerComponent.js
+++ frontend/src/components/SpeakerComponent.js
@@ -3,6 +3,8 @@ import Speaker from "./SpeakerCard";
 import "../styles/SpeakerComponent.css";
 import SpeakerImage1 from "../assets/images/speaker.jpeg";
 import SpeakerImage10 from "../assets/images/speaker10.jpg"
+import SpeakerImage11 from "../assets/images/speaker11.jpg"
+import SpeakerImage12 from "../assets/images/speaker12.jpeg"
 
 export default function Speakers() {
     return(
@@ -15,6 +17,8 @@ export default function Speakers() {
                 <div className="speakercomponent-row">
                     <Speaker image={SpeakerImage1} name={"Prof. Phaneendra Yalavarthy, IISc Bengaluru"}/>
                     <Speaker image={SpeakerImage10} name={"Mr. Dileep Mangsuli, Head, R&D Development centre, Siemens Healthineers"}/>
+                    <Speaker image={SpeakerImage11} name={"Prof. Neelam Sinha, IISc Bengaluru"}/>
+                    <Speaker image={SpeakerImage12} name={"Prof.  Prabhdeep Kaur, IISc Bengaluru"}/>
                 </div>
                 <div className="speakercomponent-row">
                     More Speakers to Come.
